9334933199b9a63379532553944a741ee02b3865
Darren Hannay <42913620+DarrenHannay2019@users.noreply.github.com>
2021-03-28 11:36:10 +0100

    Login Form
    Code added to the login form to check against the database

--- DMHStockController/DMHStockControllerV5/FLogin.cs
+++ DMHStockController/DMHStockControllerV5/FLogin.cs
@@ -16,5 +16,53 @@ namespace DMHStockControllerV5
         {
             InitializeComponent();
         }
+
+        private void CmdLogin_Click(object sender, EventArgs e)
+        {
+            TotalUsers;
+            int PassResult;
+            ClsEmployee employee = new ClsEmployee();
+            TotalUsers = employee.GetAllUserTotal();
+            PassResult = employee.GetLoginUserID(TxtUserName.Text.TrimEnd(), TxtPassword.Text.TrimEnd());
+            if (TotalUsers == 0)
+            {
+                DialogResult dialog = MessageBox.Show("Unknown User and do you wish to add new user?", Application.ProductName, MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                if (dialog == DialogResult.Yes)
+                {
+                    FrmEmployee frmEmployee = new FrmEmployee();
+                    frmEmployee.FormMode = "New";
+                    frmEmployee.ShowDialog();
+                    Application.Exit();
+                }
+                else
+                {
+                    TxtUserName.Clear();
+                    TxtPassword.Clear();
+                    TxtUserName.Select();
+                }
+            }
+            else
+            {
+                if (PassResult != 0)
+                {
+                    FrmMain frmMain = new FrmMain
+                    {
+                        RefToLoginForm = this,
+                        UserID = PassResult
+                    };
+                    frmMain.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    Application.Exit(); // unknown user need to keep system secure.
+                }
+            }
+        }
+
+        private void CmdExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();     // Exit the application
+        }
     }
 }
